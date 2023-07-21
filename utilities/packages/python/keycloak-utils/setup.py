227989f8fbc080344e59dc79430dd9e006985fac
Peter Baker <peter.baker122@csiro.au>
2023-07-21 05:22:42 +0000

    Fixing versions for pydantic and pytest-docker

--- utilities/packages/python/keycloak-utils/setup.py
+++ utilities/packages/python/keycloak-utils/setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(
         include=['KeycloakRestUtilities', 'KeycloakRestUtilities.*']),
-    install_requires=['requests', 'types-requests', 'pydantic', 'python-jose'],
+    install_requires=['requests', 'types-requests', 'pydantic==1.*', 'python-jose'],
     package_data={
         'KeycloakRestUtilities': ['py.typed']
     }
