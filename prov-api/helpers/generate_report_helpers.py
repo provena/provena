623943cc4850c2556c1ee29af018b01594ceae00
parth-kulkarni1 <parth.kulkarni2705@gmail.com>
2024-10-17 06:11:58 +0000

    Developing registry helper function to fetch loaded item and impoved error handling of backend-code.

--- prov-api/helpers/generate_report_helpers.py
+++ prov-api/helpers/generate_report_helpers.py
@@ -1,6 +1,6 @@
 import io
 import os
-from typing import Dict, List, Any, Callable, Tuple, TypedDict
+from typing import Dict, List, Any, Callable, Tuple, Set
 from dataclasses import dataclass, field
 from enum import Enum
 
@@ -24,25 +24,31 @@ class NodeType(str, Enum):
     OUTPUTS = "outputs"
 
 @dataclass
-class GenerateReportFormat():
-    inputs: List[Node] = field(default_factory=list)
-    model_runs: List[Node] = field(default_factory=list)
-    outputs: List[Node] = field(default_factory=list)
+class ReportNodeCollection():
+    inputs: Set[Node] = field(default_factory=Set)
+    model_runs: Set[Node] = field(default_factory=Set)
+    outputs: Set[Node] = field(default_factory=Set)
 
     def add_node(self, node: Node, node_type: NodeType) -> None:
+        
+        # Filter the node to remove point of origin and only include datasets and models.
+
+        # input - datasets and models 
+        # outputs - datasets
 
         # Handle the nodes of type MODEL_RUN
-        if node.item_subtype == ItemSubType.MODEL_RUN:
-            self.model_runs.append(node)
 
-        # Handle input nodes.
-        elif node_type == NodeType.INPUTS: 
-            self.inputs.append(node)
+            if node.item_subtype == ItemSubType.MODEL_RUN:
+                self.model_runs.add(node)
+
+            # Handle input nodes.
+            elif node_type == NodeType.INPUTS and node.item_subtype == ItemSubType.MODEL or node.item_subtype == ItemSubType.DATASET: 
+                self.inputs.add(node)
+            
+            # Handle output nodes.
+            elif node_type == NodeType.OUTPUTS and node.item_subtype == ItemSubType.DATASET:
+                self.outputs.add(node)
         
-        # Handle output nodes.
-        elif node_type == NodeType.OUTPUTS:
-            self.outputs.append(node)
-    
 async def validate_node_id(node_id: str, item_subtype: ItemSubType, request_style: RequestStyle, config: Config) -> None: 
     """Validates that a provided node (id + subtype) does exist within the registry.
     This only currently works with Model Runs and Study Entities. 
@@ -67,27 +73,36 @@ async def validate_node_id(node_id: str, item_subtype: ItemSubType, request_styl
     HTTPException
         If the fetched entity is of type SeededItem this exception is raised.
     """
-    
-    validation_mapping: Dict[ItemSubType, Callable] = {
-        ItemSubType.MODEL_RUN: validate_model_run_id,
-        ItemSubType.STUDY: validate_study_id
-    }
 
-    validator_func = validation_mapping.get(item_subtype)
+    try:
+        
+        validation_mapping: Dict[ItemSubType, Callable] = {
+            ItemSubType.MODEL_RUN: validate_model_run_id,
+            ItemSubType.STUDY: validate_study_id
+        }
 
-    if not validator_func:
-        raise HTTPException(status_code=400, detail="Unsupported item subtype for validation.")
-    
-    response = await validator_func(id=node_id, request_style=request_style, config=config)
+        validator_func = validation_mapping.get(item_subtype)
 
-    if isinstance(response, str):
-        raise HTTPException(status_code=400, detail=f"Validation error with provided {item_subtype.value}: {response}")
-    
-    if isinstance(response, SeededItem):
-        raise HTTPException(status_code=400, detail="Seeded item cannot be used for this query!")
-    
+        if not validator_func:
+            raise HTTPException(status_code=400, detail=f"Unsupported item subtype for validation.\
+                                Received subtype {item_subtype.value}, but the supported subtypes\
+                                are {','.join(validation_mapping.keys())}")
+        
+        response = await validator_func(id=node_id, request_style=request_style, config=config)
 
-def filter_for_export_prov_graph(upstream_nodes: List[Node], downstream_nodes: List[Node]) -> GenerateReportFormat: 
+        if isinstance(response, str):
+            raise HTTPException(status_code=400, detail=f"Validation error with provided {item_subtype.value}: {response}")
+        
+        if isinstance(response, SeededItem):
+            raise HTTPException(status_code=400, detail=f"Seeded item with provided {item_subtype.value} cannot be used for this query!")
+    
+    except HTTPException as e: 
+        raise e
+    
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=f"Error when validating study or model run entities: {str(e)}")
+
+def filter_for_export_prov_graph(upstream_nodes: List[Node], downstream_nodes: List[Node]) -> ReportNodeCollection: 
     """Takes a list of upstream and downstream nodes and processes them into into different categories of 
     inputs, models and outputs. 
 
@@ -100,11 +115,11 @@ def filter_for_export_prov_graph(upstream_nodes: List[Node], downstream_nodes: L
 
     Returns
     -------
-    Dict[str,List[Node]]
-        A dictionary containing the "inputs", "outputs" and "model-runs" involved within the study/model-run.
+    ReportNodeCollection
+        A dataclass containing the "inputs", "outputs" and "model-runs" involved within the study/model-run.
     """
 
-    filtered_response = GenerateReportFormat()
+    filtered_response = ReportNodeCollection()
 
     for node in upstream_nodes:
         filtered_response.add_node(node, NodeType.INPUTS)
@@ -141,19 +156,32 @@ def parse_nodes(node_list: List[Any]) -> List[Node]:
 
     node_list_parsed: List[Node] = []
 
-    if len(node_list) == 0: 
-        # Do I handle this? Can there be a case where a model run does not have any assoicated inputs/outputs?? - I don't think so.
-        raise HTTPException(status_code=400, detail="Cannot process the report generation.")
+    # Worst case scenario handling, this is unlikely to happen.
+    if len(node_list) == 0:
+        raise HTTPException(status_code=400, detail="Cannot process the report generation. Node list is empty.")
+    
     try:
-        for node in node_list: 
-            node_list_parsed.append(Node(**node))
-    except ValidationError as e: 
-        raise HTTPException(status_code=400, detail="Something has gone wrong in parsing this node.")
         
+        for node in node_list:
+            try: 
+                node_list_parsed.append(Node(**node))
+            except ValidationError as e:
+                # The node object has not been parsed, so not safe to get it directly. 
+                node_id = node.get('id', 'undefined id')
+                raise HTTPException(status_code=400, detail=f"Validation error in node with id {node_id}. Error {str(e)}")
+            except Exception as e: 
+                raise HTTPException(status_code=500, detail=f"Error parsing nodes - {str(e)}")
+    
+    except HTTPException as e:
+        raise e
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error during node parsing - {str(e)}")
+    
     return node_list_parsed
 
 
-def generate_report(starting_id: str, upstream_depth: int, config: Config) -> GenerateReportFormat:
+def generate_report(starting_id: str, upstream_depth: int, config: Config) -> ReportNodeCollection:
     """Generates the report by querying upstream and downstream data, parsing nodes, and filtering results.
 
     This helper function performs upstream and downstream queries, parses the node responses, filters them 
@@ -186,75 +214,80 @@ def generate_report(starting_id: str, upstream_depth: int, config: Config) -> Ge
 
 
 def fetch_parse_all_upstream_downstream_nodes(starting_id: str, upstream_depth: int, config: Config, downstream_depth: int = 1) -> Tuple[List[Node], List[Node]] : 
-
-    print(starting_id, "starting-id")
-    print(upstream_depth, "upstream-depth")
     
-    upstream_response: Dict[str, Any] = upstream_query(starting_id=starting_id, depth=upstream_depth, config=config)
-    downstream_response: Dict[str, Any] = downstream_query(starting_id=starting_id, depth=downstream_depth, config=config)
+    try:
+        
+        upstream_response: Dict[str, Any] = upstream_query(starting_id=starting_id, depth=upstream_depth, config=config)
+        downstream_response: Dict[str, Any] = downstream_query(starting_id=starting_id, depth=downstream_depth, config=config)
 
-    print(upstream_response, downstream_response)
+        assert upstream_response.get('nodes'), "Upstream node collections not found!"
+        assert downstream_response.get('nodes'), "Downstream node collections not found!"
 
-    assert upstream_response.get('nodes'), "Node collections not found!"
-    assert downstream_response.get('nodes'), "Node collections not found!"
+        nodes_upstream: List[Any] = upstream_response.get('nodes', [])
+        nodes_downstream: List[Any] = downstream_response.get('nodes', [])
 
-    nodes_upstream: List[Any] = upstream_response.get('nodes', [])
-    nodes_downstream: List[Any] = downstream_response.get('nodes', [])
+        # Parsing the nodes.
+        upstream_nodes_parsed: List[Node] = parse_nodes(nodes_upstream)
+        downstream_nodes_parsed: List[Node] = parse_nodes(nodes_downstream)
 
-    # Parsing the nodes.
-    upstream_nodes_parsed: List[Node] = parse_nodes(nodes_upstream)
-    downstream_nodes_parsed: List[Node] = parse_nodes(nodes_downstream)
+    except AssertionError as e: 
+        raise HTTPException(status_code=404, detail=f"The provided model run with id {starting_id} - error: {str(e)}")
+    
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=f"Error fetching upstream/downstream nodes - error: {str(e)}")
 
     return upstream_nodes_parsed, downstream_nodes_parsed
 
-def generate_word_file(node_collection: GenerateReportFormat) -> str: 
+def generate_word_file(node_collection: ReportNodeCollection) -> str: 
     """Generates and creates a temporary word file using Python-docx.
-    TODO: Need to add the list of collected nodes into here for processing into table.
-
+   
     Returns
     -------
     str
         The newly created temporary file path.
     """
 
-    # Need to convert/parse into a suitable format that can be iterated through. 
-    document = Document()
-    document.add_heading('Model Run Study Close Out Report', 0)
+    try:
+        # Need to convert/parse into a suitable format that can be iterated through. 
+        document = Document()
+        document.add_heading('Model Run Study Close Out Report', 0)
 
-    table = document.add_table(rows=4, cols=2)
-    table.style = 'Table Grid'
+        table = document.add_table(rows=4, cols=2)
+        table.style = 'Table Grid'
 
-    # First row here is the modelling-team-name
-    table.cell(0,0).text = "Modelling Team"
-    table.cell(0,1).text = "C~Scape"
+        # First row here is the modelling-team-name
+        table.cell(0,0).text = "Modelling Team"
+        table.cell(0,1).text = "C~Scape"
 
-    # Second row here is the inputs
-    input_row = table.rows[1].cells[0]
-    input_row.text = "Inputs:"
-    input_data_cell = table.cell(1,1)
-    for input_node in node_collection.inputs:
-        input_data_cell.text += f"\n - {input_node.id}: {input_node.item_category}"
+        # Second row here is the inputs
+        input_row = table.rows[1].cells[0]
+        input_row.text = "Inputs:"
+        input_data_cell = table.cell(1,1)
+        for input_node in node_collection.inputs:
+            input_data_cell.text += f"\n - {input_node.id}: {input_node.item_category}"
 
-    # Third row here is the model runs
-    model_run_row = table.rows[2].cells[0]
-    model_run_row.text = "Model Runs:" 
-    model_run_row_data_cell = table.cell(2,1)
-    for model_run_node in node_collection.model_runs:
-        model_run_row_data_cell.text += f"\n - {model_run_node.id}: {model_run_node.item_category}"
+        # Third row here is the model runs
+        model_run_row = table.rows[2].cells[0]
+        model_run_row.text = "Model Runs:" 
+        model_run_row_data_cell = table.cell(2,1)
+        for model_run_node in node_collection.model_runs:
+            model_run_row_data_cell.text += f"\n - {model_run_node.id}: {model_run_node.item_category}"
 
-    # Fourth row here is the outputs
-    output_row = table.rows[3].cells[0]
-    output_row.text = "Outputs:"
-    output_row_data_cell = table.cell(3,1)
-    for output_node in node_collection.inputs:
-        output_row_data_cell.text += f"\n - {output_node.id}: {output_node.item_category}"
+        # Fourth row here is the outputs
+        output_row = table.rows[3].cells[0]
+        output_row.text = "Outputs:"
+        output_row_data_cell = table.cell(3,1)
+        for output_node in node_collection.inputs:
+            output_row_data_cell.text += f"\n - {output_node.id}: {output_node.item_category}"
 
-    # Temporarily save the document to return it. 
-    with NamedTemporaryFile(delete=False, suffix='.docx') as tmpFile: 
-        document.save(tmpFile.name)
-        # Return the path of the temporary file.
-        return tmpFile.name
-
+        # Temporarily save the document to return it. 
+        with NamedTemporaryFile(delete=False, suffix='.docx') as tmpFile: 
+            document.save(tmpFile.name)
+            # Return the path of the temporary file.
+            return tmpFile.name
+           
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=f"Error generating the word file: {str(e)}")
 
 def remove_file(file_path: str) -> None: 
     """Deletes the specified file from the server.
@@ -265,18 +298,31 @@ def remove_file(file_path: str) -> None:
         The file path of the document to be deleted.
     """
 
-    os.remove(file_path)
+    try:
+        os.remove(file_path)
 
+    except FileNotFoundError as e: 
+        raise HTTPException(status_code=500, detail=f"Unable to delete the file with path {file_path} - {str(e)}")
+    
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=f"Something has gone wrong in deleting the file {str(e)}")
 
-def get_model_runs_from_study(node_id:str, config:Config, depth:int = 1) -> List[Node]: 
+def get_model_runs_from_study(study_node_id:str, config:Config, depth:int = 1) -> List[Node]: 
+    
+    try:
+        # Query downstream to get all linked model runs at depth 1. 
+        # This may contain more than one model run.
+        downstream_model_run_response = downstream_query(starting_id=study_node_id, depth=depth, config=config)
+        assert downstream_model_run_response.get('nodes'), "Downstream node collections not found!"
 
-    # Query downstream to get all linked model runs at depth 1. 
-    # This may contain more than one model run.
-    downstream_model_run_response = downstream_query(starting_id=node_id, depth=depth, config=config)
-    assert downstream_model_run_response.get('nodes'), "Node collections not found!"
-
-    model_run_nodes: List[Node] = parse_nodes(downstream_model_run_response.get('nodes', []))
-    return model_run_nodes
+        model_run_nodes: List[Node] = parse_nodes(downstream_model_run_response.get('nodes', []))
+        return model_run_nodes
+    
+    except AssertionError as e:
+        raise HTTPException(status_code=404, detail=f"The provided study run with id {study_node_id} - error: {str(e)}")
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching downstream nodes from the provided study with id {study_node_id} - error: {str(e)}")
 
 async def generate_report_helper(
         node_id: str, 
@@ -286,41 +332,49 @@ async def generate_report_helper(
         config: Config
 ) -> str: 
     
-    # Create the request style, here we assert where the HTTP request came from. 
-    request_style: RequestStyle = RequestStyle(
-        user_direct=roles.user, service_account=None
-    )
+    try:
+        
+        # Create the request style, here we assert where the HTTP request came from. 
+        request_style: RequestStyle = RequestStyle(
+            user_direct=roles.user, service_account=None
+        )
 
-    report_nodes: GenerateReportFormat = GenerateReportFormat()
+        report_nodes: ReportNodeCollection = ReportNodeCollection()
 
-    # do checks accordingly. 
-    await validate_node_id(node_id=node_id, item_subtype=item_subtype, request_style=request_style, config=config)
+        # do checks accordingly. 
+        await validate_node_id(node_id=node_id, item_subtype=item_subtype, request_style=request_style, config=config)
 
-    if item_subtype == ItemSubType.MODEL_RUN: 
-        report_nodes = generate_report(starting_id=node_id, upstream_depth=upstream_depth, config=config)
+        if item_subtype == ItemSubType.MODEL_RUN: 
+            report_nodes = generate_report(starting_id=node_id, upstream_depth=upstream_depth, config=config)
 
-    elif item_subtype == ItemSubType.STUDY: 
+        elif item_subtype == ItemSubType.STUDY: 
 
-        # Get all model runs associated with the study.
-        model_run_nodes = get_model_runs_from_study(node_id=node_id, config=config)
+            # Get all model runs associated with the study.
+            model_run_nodes = get_model_runs_from_study(study_node_id=node_id, config=config)
 
-        # Now branch out with the model runs in here and explore them as above (depth 1-3 upstream and depth 1 downstream)
-        # The model run will be the new updated starting id.
-        for model_run_node in model_run_nodes:
-            if model_run_node.item_subtype == ItemSubType.MODEL_RUN:
-                model_run_id = model_run_node.id
-                # Extract the upstream and downstream entities from this node. 
-                report_nodes = generate_report(starting_id=model_run_id, upstream_depth=upstream_depth, config=config)                
+            # Now branch out with the model runs in here and explore them as above (depth 1-3 upstream and depth 1 downstream)
+            # The model run will be the new updated starting id.
+            for model_run_node in model_run_nodes:
+                if model_run_node.item_subtype == ItemSubType.MODEL_RUN:
+                    model_run_id = model_run_node.id
+                    # Extract the upstream and downstream entities from this node. 
+                    report_nodes = generate_report(starting_id=model_run_id, upstream_depth=upstream_depth, config=config)                
 
-    else: 
-        raise HTTPException(status_code=400, detail="Unsupported node requested.")
+        else: 
+            raise HTTPException(status_code=400, detail=f"Unsupported node with item subtype {item_subtype.value} requested.
+                                This endpoint only supports subtype of STUDY and MODEL_RUN.")
 
-    # All the nodes involved in the model run/study have been populated. 
-    generated_doc_path = generate_word_file(report_nodes)
+        # All the nodes involved in the model run/study have been populated. 
+        generated_doc_path = generate_word_file(report_nodes)
+        
+        if os.path.exists(generated_doc_path): 
+            return generated_doc_path
+        else: 
+            raise HTTPException(status_code=400, detail= "Error generating your study-closeout document")
+        
+    except HTTPException as e: 
+        raise e 
     
-    if os.path.exists(generated_doc_path): 
-        return generated_doc_path
-    else: 
-        raise HTTPException(status_code=400, detail= "Error generating your study-closeout document")
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=f"Error in report generation: {str(e)}")     
 
-    
